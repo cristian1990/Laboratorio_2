da353d5958106393486e3919646e02fe296a47e0
cristian1990 <cristian___777@hotmail.com>
2018-01-04 15:40:16 -0300

    Segundo Parcial
    Segundo Parcial

---
+++ Segundo Parcial/Correo/Apellido.Nombre.Div/EntidadesHechas/GuardaString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesHechas
+{
+    public static class GuardaString
+    {
+
+        static string archivo = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\archivo.txt";
+
+        public static bool Guardar(this string texto, string dato)
+        {
+            StreamWriter file = null;
+            try
+            {
+                if (File.Exists(archivo))
+                    file = new StreamWriter(archivo, true);
+                else
+                    file = new StreamWriter(archivo);
+
+                file.WriteLine(dato);
+                
+            }
+            catch (IOException ex)
+            {
+                throw new IOException("Error al guardar en la ruta " + archivo, ex);
+            }
+            finally
+            {
+                file.Close();
+            }
+            return true;
+        }
+    }
+}
---
+++ Segundo Parcial/Correo/Apellido.Nombre.Div/TestUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63afdc35-1d9d-4466-9d80-8ece8bf84391")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
